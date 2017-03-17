8585ecc617b2f8b231002d5a4bc790a06eb9f775
Kasun <kmadushanka40@gmail.com>
2017-03-17 22:57:27 +0530

    first commit

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ var express = require('express');
 var app = express();
 var sql = require('mssql');
 
-app.get('/', function(req, res) {
-    res.send('Hello Worlddw!')
-})
-
-app.listen(process.env.PORT||80, function() {
-    console.log('Example app listening on port 3000!')
-})
-
 var config = {
 
     user: 'kasun@trainmate',
@@ -21,12 +13,30 @@ var config = {
     }
 }
 
-sql.connect(config).then(function() {
-    console.log('opening connection');
-    new sql.Request().query('Select * from customers').then(function(recordset) {
+app.get('/', function(req, res) {
+    res.send('Hello Worlddw!')
 
-        console.dir(recordset);
-    }).catch(function(error) {
+    sql.connect(config).then(function() {
+        console.log('opening connection');
+        new sql.Request().query('Select * from customers').then(function(recordset) {
 
+            res.send('Hello Worlddw!')
+        }).catch(function(error) {
+
+        });
     });
+
+})
+
+app.listen(process.env.PORT||80, function() {
+    console.log('Example app listening on port 3000!')
+})
+
+
+
+app.use(express.bodyParser());
+
+app.post('/', function(request, response){
+    console.log(request.body.user.name);
+    console.log(request.body.user.email);
 });
