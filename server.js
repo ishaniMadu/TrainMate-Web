8bf0082700b6a2181cfb856ee56c086e90d1a901
Kasun <kmadushanka40@gmail.com>
2017-03-18 10:44:29 +0530

    first commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:80');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
