12f26a722f23ae7cf6382da9e67fd3d77305501f
Kasun <kmadushanka40@gmail.com>
2017-03-19 21:26:36 +0530

    latest commit

--- www.js
+++ www.js
@@ -4,7 +4,7 @@
 * Module dependencies.
 */
 
-var app = require('app');
+var app = require('./app');
 var debug = require('debug')('trainmate:server');
 var http = require('http');
 var socketio = require('socket.io');
