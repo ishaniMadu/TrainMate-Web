edad99cd67eb3256f5d3090db8d486a4dbb4f8dd
Kasun <kmadushanka40@gmail.com>
2017-03-19 21:48:13 +0530

    latest commit

--- www.js
+++ www.js
@@ -1,8 +1,7 @@
 
-var app = require('./app');
+var app = require('app');
 var debug = require('debug')('trainmate:server');
 var http = require('http');
-var bodyParser = require('body-parser');
 var socketio = require('socket.io');
 
 /**
@@ -31,51 +30,6 @@ server.on('listening', onListening);
 
 //var debug = false;
 initSocketIO(server,false);
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-app.use(bodyParser.json());
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.post('/', urlencodedParser, function(req, res) {
-
-    var email= req.body.email;
-    var password= req.body.password;
-
-    sql.connect(connection).then(function() {
-        console.log('opening connection');
-        new sql.Request().query("Select * from customers WHERE NAME='"+email+"'").then(function(recordset) {
-            console.dir(recordset);
-            if(recordset.length>0){
-                res.send(recordset);
-
-            }else{
-
-            }
-        }).catch(function(error) {
-
-        });
-    });
-
-})
 
 /**
 * Normalize a port into a number, string, or false.
